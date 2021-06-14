81bb034c24b8d893e99d190653dc96fead56eb35
Ivan Enchev <63471679+3nch3v@users.noreply.github.com>
2021-06-14 10:10:06 +0200

    Quicksort inplemention

---
+++ SortingAlgorithms/Quicksort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    public static class Quicksort
+    {
+        private const string byAscending = "ascending";
+        private const string byDesending = "desending";
+
+        public static int[] OrderByAscending(int[] input)
+        {
+            int[] result = CopyInput(input);
+            Sort(result, 0, input.Length - 1, byAscending);
+            return result;
+        }
+
+        public static int[] OrderByDescending(int[] input)
+        {
+            int[] result = CopyInput(input);
+            Sort(result, 0, input.Length - 1, byDesending);
+            return result;
+        }
+
+        private static void Sort(int[] arr, int left, int right, string orderType)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right, orderType);
+
+                Sort(arr, left, pivot - 1, orderType);
+                Sort(arr, pivot + 1, right, orderType);
+            }
+        }
+
+        private static int Partition(int[] arr, int left, int right, string orderType)
+        {
+            int pivot = arr[right];
+            int index = left;    
+
+            for (int j = left; j < right; j++)
+            {
+                if (orderType == byAscending)
+                {
+                    if (arr[j] <= pivot)
+                    {
+                        SwapNumbers(arr, index, j);
+                        index++;
+                    }
+                }
+                else if(orderType == byDesending)
+                {
+                    if (arr[j] >= pivot)
+                    {
+                        SwapNumbers(arr, index, j);
+                        index++;
+                    }
+                }
+            }
+
+            SwapNumbers(arr, index, right);
+            return index;
+        }
+
+        private static void SwapNumbers(int[] arr, int index, int j)
+        {
+            int temp = arr[index];
+            arr[index] = arr[j];
+            arr[j] = temp;
+        }
+
+        private static int[] CopyInput(int[] arr)
+        {
+            int[] copy = new int[arr.Length];
+            Array.Copy(arr, copy, arr.Length);
+            return copy;
+        }
+    }
+}
--- SortingAlgorithms/Startup.cs
+++ SortingAlgorithms/Startup.cs
@@ -5,30 +5,42 @@
     {
         static void Main(string[] args)
        {
-            int[] arr = new[] { 68, 235, 1, 12, 1231234, 2, 10, 4, 33, 1, 6, 8, 5, 3, 100, 100032, 123, 23, 233, 2234, 4567 };
+            int[] arr = new[] { 68, 235, 1, 12, -2, 0, 1, 1231234, 2, 10, 4, 33, 1, 6, 8, 5, 3, 100, 100032, 123, 23, 233, 2234, 4567 };
 
             Console.WriteLine("Original order:");
             Console.WriteLine(string.Join(" ", arr));
             Console.WriteLine();
 
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("BubleSort by ascending:");
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine(string.Join(" ", BubbleSort.OrderByAscending(arr)));
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("BubleSort by desending:");
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine(string.Join(" ", BubbleSort.OrderByDesending(arr)));
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("BubleSort by ascending:");
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine(string.Join(" ", BubbleSort.OrderByAscending(arr)));
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("BubleSort by desending:");
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine(string.Join(" ", BubbleSort.OrderByDesending(arr)));
 
-            Console.WriteLine();
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("Mergesort by ascending:");
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine(string.Join(" ", MergeSort.OrderByAscending(arr)));
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine("Mergesort by desending:");
-            Console.WriteLine("-----------------------------------------------");
-            Console.WriteLine(string.Join(" ", MergeSort.OrderByDesending(arr)));
+            //Console.WriteLine();
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("Mergesort by ascending:");
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine(string.Join(" ", MergeSort.OrderByAscending(arr)));
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("Mergesort by desending:");
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine(string.Join(" ", MergeSort.OrderByDesending(arr)));
+
+            //Console.WriteLine();
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("Quicksort by ascending:");
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine(string.Join(" ", Quicksort.OrderByAscending(arr)));
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine("Quicksort by desending:");
+            //Console.WriteLine("-----------------------------------------------");
+            //Console.WriteLine(string.Join(" ", Quicksort.OrderByDescending(arr)));
+
+
         }
     }
 }
